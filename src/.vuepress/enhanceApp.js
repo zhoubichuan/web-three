f34b5fbeb2c7c9823b1ffd0f6eb7ed0346a77979
zhoubichuan <zhoubichuan@icloud.com>
2022-03-01 23:07:40 +0800

    master

--- src/.vuepress/enhanceApp.js
+++ src/.vuepress/enhanceApp.js
@@ -1,3 +1,8 @@
 // 通过该文件，把需要用的组件进行全局的注册，因为在markdown中的文件不能使用import引入组件，必须要提前注册好全局的组件
 import * as three from 'three'
-window.THREE = three
+
+export default ({
+    Vue
+}) => {
+    Vue.prototype.THREE = three
+}
