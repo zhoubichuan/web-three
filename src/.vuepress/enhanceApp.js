fc0e25af70acb73cd729a9c77c3600600da70450
zhoubichuan <zhoubichuan@icloud.com>
2022-03-16 00:21:34 +0800

    master

--- src/.vuepress/enhanceApp.js
+++ src/.vuepress/enhanceApp.js
@@ -1,15 +1,5 @@
-import utils from '../base/engine/utils.vue';
-import project from '../base/engine/project.vue';
-import vuecli from '../base/engine/vuecli.vue';
-import env from '../base/engine/env.vue';
-import ui from '../base/engine/ui.vue';
-
 export default ({
     Vue
 }) => {
-    Vue.component('engineutils', utils);
-    Vue.component('engineproject', project);
-    Vue.component('enginevuecli', vuecli);
-    Vue.component('engineenv', env);
-    Vue.component('engineui', ui);
+
 }
