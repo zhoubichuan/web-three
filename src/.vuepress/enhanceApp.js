e7ebe73354b1e73e93b1e4bdb258dc6c382116d1
周必川 <zhoubichuan@icloud.com>
2023-07-16 16:58:14 +0800

    update

--- src/.vuepress/enhanceApp.js
+++ src/.vuepress/enhanceApp.js
@@ -1,7 +1,12 @@
-import xminder from './xmind.vue';
-
-export default ({
-    Vue
-}) => {
-    Vue.component('xminder', xminder);
-}
+import config from "@/config";
+import ui from "@/ui";
+// import directive from "@/directive";
+// import store from "@/store";
+// import i18n from "@/i18n";
+export default ({ Vue }) => {
+//   Vue.use(directive);
+  Vue.use(ui);
+  Vue.use(config);
+//   Vue.use(store);
+//   Vue.use(i18n);
+};
